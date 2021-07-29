ce604780d1c76518f190b8fa18d94de35ffc1466
LeagueLugas <leaguelugas@gmail.com>
2021-07-29 14:37:32 +0900

    Add : cashbook response타입과 cashbook 타입

--- client/src/types.ts
+++ client/src/types.ts
@@ -17,3 +17,30 @@ export interface CalendarDate {
   thisMonthLastDate: Date;
   nextMonthFirstDate: Date;
 }
+
+export type CashcooksResponse = {
+  totalElements: number;
+  totalIncome: number;
+  totalExpenditure: number;
+  cashbooks: CashbookType[];
+};
+
+export type CashbookType = {
+  id?: number;
+  category:
+    | '식비'
+    | '생활'
+    | '쇼핑/뷰티'
+    | '교통'
+    | '의료/건강'
+    | '문화/여가'
+    | '미분류'
+    | '월급'
+    | '용돈'
+    | '기타수입';
+  categoryType: 'expenditure' | 'income';
+  memo: string;
+  payment: string;
+  price: number;
+  date: string;
+};
