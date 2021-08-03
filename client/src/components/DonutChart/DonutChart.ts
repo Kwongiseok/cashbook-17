19033ab36964341de57b0ab2ecf0f027aff0bc47
Kwongiseok <ri2377@naver.com>
2021-08-03 22:04:29 +0900

    Fix : 더미 데이터 삭제

--- client/src/components/DonutChart/DonutChart.ts
+++ client/src/components/DonutChart/DonutChart.ts
@@ -3,16 +3,6 @@ import { ExpenditureDataList, HistoryState, MainChartState } from '../../types';
 import Component from '../../utils/Component';
 import './donutChart.scss';
 
-const dummy = [
-  { category: '생활', percent: 64, total: 536460 },
-  { category: '의료/건강', percent: 15, total: 125300 },
-  { category: '쇼핑/뷰티', percent: 7, total: 56000 },
-  { category: '교통', percent: 6, total: 45340 },
-  { category: '식비', percent: 5, total: 40540 },
-  { category: '문화/여가', percent: 2, total: 20800 },
-  { category: '미분류', percent: 1, total: 10200 },
-];
-
 export default class DonutChart extends Component<MainChartState> {
   constructor($target: HTMLElement, state: MainChartState) {
     super($target, state);
